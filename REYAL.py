1374d56163c81bc48281d20976c697318b4efbf3
SiBloxIto <abaydominicjohn@gmail.com>
2024-10-10 19:09:27 +0800

    Update REYAL.py

--- REYAL.py
+++ REYAL.py
@@ -1,19 +1,3 @@
-# Simple Login System
-
-# Predefined username and password
-correct_username = "pogi"
-correct_password = "ako"
-
-# Prompt the user for input
-username = input("Enter username: ")
-password = input("Enter password: ")
-
-# Check if the entered username and password are correct
-if username == correct_username and password == correct_password:
-    print("Login successful! Welcome!")
-else:
-    print("Invalid username or password. Please try again.")
-
 # Open Source By SYED-ZADA
 
 fbks=('com.facebook.adsmanager','com.facebook.lite','com.facebook.orca','com.facebook.katana')
